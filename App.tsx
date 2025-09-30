796b053b4f1ec35eaa58648a6cd9f0bd38d0d6a6
kristiangarcia <kristiangarciapaulsen@gmail.com>
2025-09-30 21:03:00 +0200

    Import Platform

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Platform, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { LinearGradient } from 'expo-linear-gradient'
 import { MaterialIcons } from '@expo/vector-icons'
